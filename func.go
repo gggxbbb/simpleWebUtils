aab397d748a082d946c6e22d0df16d730e7566dc
gggxbbb <2331490629@qq.com>
2023-08-18 10:02:56 +0800

    松散化

---
+++ func.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func echoCode(c *gin.Context) {
+	code := c.Param("code")
+	// parse code to int
+	intCode, err := strconv.Atoi(code)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "invalid code",
+		})
+		return
+	}
+	c.Data(intCode, "", nil)
+}
