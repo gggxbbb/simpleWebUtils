c683bc900711775c296a1ba7fba2b6c0f1d3f34b
gggxbbb <2331490629@qq.com>
2023-08-18 10:17:57 +0800

    加入 readme

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	_ "embed"
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed readme.html
+var readmeHTML string
 
 func main() {
 
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.Data(200, "text/html", []byte(readmeHTML))
+	})
 
 	r.GET("/echo/code/:code", echoCode)
 	r.GET("/echo/ua", echoUA)
